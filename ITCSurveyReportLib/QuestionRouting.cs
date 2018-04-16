3a7c6bec436f2437ce7e33c5c05083ef98d463b2
eddiebauer0 <eddiebauer0@gmail.com>
2018-04-16 14:01:45 -0400

    updated QuestionRouting

--- ITCSurveyReportLib/QuestionRouting.cs
+++ ITCSurveyReportLib/QuestionRouting.cs
@@ -9,13 +9,15 @@ using System.Text.RegularExpressions;
 namespace ITCSurveyReport
 {
     /// <summary>
-    /// Represents the routing/skips portion of a survey question, also known as Post-programming instructions.
+    /// Represents the routing/skips portion of a survey question, also known as Post-programming instructions. This object also
+    /// includes the response option set for the question.
     /// </summary>
     class QuestionRouting
     {
-        string routingText;  // the complete text of the filter
-        bool hasVar;        // true if the filter contains a variable name
-        List<FilterVar> filterVars;      // list of varnames that appear in this filter
+        string routingText;  // the complete text of the routing
+        string [] responseOptions; // the response options for the question
+        bool hasVar;        // true if the routing contains a variable name
+        List<FilterVar> routingVars;      // list of varname destinations that appear in this routing instruction
 
         public QuestionRouting()
         {
@@ -31,134 +33,264 @@ namespace ITCSurveyReport
 
         }
 
-        public QuestionRouting(string filter)
+        public QuestionRouting(string routing, string respOptions)
         {
-            string var = Utilities.ExtractVarName(filter);
-            routingText = filter;
+            string var = Utilities.ExtractVarName(routing);
+            routingText = routing;
+            responseOptions = respOptions.Split('\r','\n');
             // check for vars to set hasVar flag
             if (!var.Equals(""))
                 hasVar = true;
             // populate filterVars list
-            filterVars = new List<FilterVar>();
+            routingVars = new List<FilterVar>();
             GetRoutingVars();
         }
 
+        /// <summary>
+        /// Isolates the variable names in the routing expression and adds them to the list of 
+        /// </summary>
         private void GetRoutingVars()
         {
-            string filterVar;
+            string r;
+            string[] routing;
+            string[] options;
+            string[] routingNumbers;
+            string destination;
+            string numbers;
+            string[] numbersArray;
+            RoutingType routingType;
+            string respNum;
+            string finalRouting;
+            Regex rx = new Regex("go to ([A-Z][A-Z][A-Z]/|[0-9][0-9][0-9][a-z]*/)*[a-zA-z][a-zA-z](\\d{5}|\\d{3})");
+            MatchCollection results;
+            Match m;
+            string routingVar;
 
-
-            int filterVarPos;
-            int filterVarLen;
+            int routingVarPos;
+            int routingVarLen;
 
             string routingExp; // the routing expression of the variable e.g. If response = [list], go to [varname].
 
-            string[] filterOptionsList;
-            string options;
+            string[] routingOptionsList;
+ 
+            // split the routing and options into arrays
+            
+            routing = routingText.Split(new string[] { "<br>" }, StringSplitOptions.RemoveEmptyEntries);
 
-
-            Regex rx;
-            MatchCollection results;
-
-
-            while (!routingText.Equals(""))
+            for (int i = 0; i < routing.Length; i++)
             {
-                FilterVar fv;
-                filterVar = Utilities.ExtractVarName(routingText);
+                // get routing type, if 1 or 2, this instruction will be removed from the routing field and its routing destination will be
+                // appended to the appropriate response option, if 3, this routing may be moved to the response option location
+                routingType = GetRoutingType(routing[i]);
 
-                if (filterVar.Equals(""))
-                    break;
+                // start with the destination
+                results = rx.Matches(routing[i]);
+                // go to next line of routing if there is no match for our pattern
+                if (results.Count == 0)
+                    continue;
 
-                rx = new Regex(filterVar + "(=|<|>|<>)" +
-                            "(([0-9]+(,\\s[0-9]+)+\\sor\\s[0-9]+)" +
-                            "|([0-9]+\\sor\\s[0-9]+)" +
-                            "|([0-9]+\\-[0-9]+)" +
-                            "|([0-9]+))");
+                m = results[0];
 
-                filterVarPos = routingText.IndexOf(filterVar);
-                filterVarLen = filterVar.Length;
+                // the destination varname (or sometimes, section) (anything after the destination variable is formatting with a smaller font)
+                destination = routing[i].Substring(m.Index, m.Length + 1) + "<Font Size=8>" + routing[i].Substring(m.Index + m.Length + 1) + "</Font>";
 
-
-                results = rx.Matches(routingText);
-
-                if (results.Count > 0)
+                switch(routingType)
                 {
-                    routingExp = results[0].Value;
-                    options = routingExp.Substring(filterVarLen + 1);
-                    options = Regex.Replace(options, "[^0-9 <->]", "");
+                    case RoutingType.IfResponse:
+                        
+                    
+                        /* the most common style (If response)
+                         get the numbers referenced by this instruction
+                         this list of numbers should be all the numbers that would route to this instruction's destination
+                        */
 
-                    filterOptionsList = GetOptionList(options).Split(' ');
+                        numbers = GetRoutingNumbers(routing[i]);
+                        numbersArray = numbers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    fv = new FilterVar();
-                    fv.Varname = filterVar;
-                    if (filterOptionsList.Length != 0)
-                    {
-                        fv.ResponseCodes = filterOptionsList.Select(Int32.Parse).ToList();
-                    }
-                    // add to the list of filter vars if it is not already there
-                    if (!filterVars.Contains(fv))
-                        filterVars.Add(fv);
+                        // now for each option, check if it starts with one of the referenced numbers
+                        // if so, add it to the routingNumbers array, and clear this instruction from the PstP
+                        break;
+                    case RoutingType.Otherwise:
+                        break;
+                    case RoutingType.If:
+                        break;
+                    case RoutingType.Other:
+                        break;
 
                 }
 
-
-                routingText = routingText.Substring(filterVarPos + filterVarLen);
             }
-
-
-
         }
 
-
-        public string GetOptionList(string options)
+        /// <summary>
+        /// Returns a list of numbers separated by a space that exist between "If response" and "go to"
+        /// </summary>
+        /// <param name="routingInstruction"></param>
+        /// <returns></returns>
+        private string GetRoutingNumbers(string routingInstruction)
         {
-            string low, high;
-            string list = "";
-            string ro;
+            
+            string routingNumbers;
+            string[] numberArr;
+            string oper;
+            string numberList = "";
+            int ifResponsePos = routingInstruction.IndexOf("If response");
+            int gotoPos = routingInstruction.IndexOf("go to");
+            // exit if If response and go to are not present
+            if (ifResponsePos == 0 && gotoPos == 0)
+                return "";
 
-            if (options.IndexOf('-') > 0)
+
+
+            routingNumbers = routingInstruction.Substring(ifResponsePos, gotoPos);
+            routingNumbers = Regex.Replace(routingNumbers, "[^0-9 =<->,]", "");
+            // get the operation before the list of numbers (=, >, or <)
+            oper = GetOperation(routingNumbers);
+            // check for range
+            if (routingNumbers.Contains("-"))
             {
-                low = options.Substring(options.IndexOf('-') - 1, 1);
-                high = options.Substring(options.IndexOf('-') + 1, 1);
-
-                for (int i = Int32.Parse(low); i < Int32.Parse(high); i++)
-                {
-                    list += Convert.ToString(i);
-                    if (i != Int32.Parse(high))
+                routingNumbers = FillRange(routingNumbers);
+            }
+            routingNumbers = Regex.Replace(routingNumbers, ",", " ");
+            numberArr = routingNumbers.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            // depending on the operation, get the numbers that will lead to this routing instruction
+            switch (oper)
+            {
+                case "=":
+                    numberList = Regex.Replace(routingNumbers, " ", ",");
+                    break;
+                case ">":
+                    for (int i = 0; i < responseOptions.Length; i++)
                     {
-                        list += " ";
+                        for (int j = 0; j < numberArr.Length; j++)
+                        {
+                            if (Int32.Parse(numberArr[j]) < ResponseNumber(responseOptions[i]))
+                                numberList += "," + numberArr[j];
+                        }
                     }
+                    break;
+                case "<":
+                    for (int i = 0; i < responseOptions.Length; i++)
+                    {
+                        for (int j = 0; j < numberArr.Length; j++)
+                        {
+                            if (Int32.Parse(numberArr[j]) < ResponseNumber(responseOptions[i]))
+                                numberList += "," + numberArr[j];
+                        }
+                    }
+                    break;
+                case "<>":
+                    for (int i = 0; i < responseOptions.Length; i++)
+                    {
+                        for (int j = 0; j < numberArr.Length; j++)
+                        {
+                            if (Int32.Parse(numberArr[j]) != ResponseNumber(responseOptions[i]))
+                                numberList += "," + numberArr[j];
+                        }
+                    }
+                    break;
 
+                default:
+                    break;
+            }
+
+            return numberList;
+        }
+        
+        /// <summary>
+        /// Returns the non-numeric characters from beginning of the string.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        private string GetOperation(string s)
+        {
+            string ineq= "";
+            double d = 0;
+            while (!Double.TryParse(s.Substring(1, 1), out d)) {
+                ineq += s.Substring(1,1);
+                s = s.Substring(2);
+            }
+            return ineq;
+        }
+
+        /// <summary>
+        /// Given an expression containing a dash surrounded by numbers, this method fills in the intermediate numbers. 
+        /// </summary>
+        /// <param name="numbers"></param>
+        /// <returns></returns>
+        public string FillRange(string numbers)
+        {
+            int low;
+            int high;
+            string[] arr;
+
+            arr = numbers.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);  // split the list into its members
+
+
+            for (int i = 0; i < arr.Length; i++) { 
+                if (arr[i].Contains('-')) { // for any range, change this member of the array into a sub-list of numbers
+                    low = Int32.Parse(arr[i].Substring(0, arr[i].IndexOf("-") - 1)); // low end of range
+                    high = Int32.Parse(arr[i].Substring(arr[i].IndexOf("-") + 1)); // high end of range
+                    arr[i] = "";
+                    for (int x = low; x < high; x++) {
+                        arr[i] = arr[i] + x + ",";
+                    }
+                    arr[i] = Utilities.TrimString(arr[i], ",");
                 }
             }
-            else if (options.StartsWith("<>"))
+            // join the array back into a string
+            return string.Join(",",arr);
+        }
+
+        /// <summary>
+        /// Get routing type, if 1 or 2, this instruction will be removed from the routing field and its routing destination will be
+        /// appended to the appropriate response option, if 3, this routing may be moved to the response option location
+        /// </summary>
+        /// <param name="routingExpression"></param>
+        /// <returns></returns>
+        public RoutingType GetRoutingType (string routingExpression)
+        {
+            RoutingType routingType;
+            if (routingExpression.StartsWith("If response"))
             {
-                list = options.Substring(2);
+                routingType = RoutingType.IfResponse;
             }
-            else if (options.StartsWith(">"))
+            else if (routingExpression.StartsWith("Otherwise"))
             {
-                list = options.Substring(1);
+                routingType = RoutingType.Otherwise;
             }
-            else if (options.StartsWith("<"))
+            else if (routingExpression.StartsWith("If"))
             {
-                list = options.Substring(1);
+                routingType = RoutingType.If;
             }
             else
             {
-                list = options;
+                routingType = RoutingType.Other;
             }
-
-            while (list.IndexOf("  ") > 0)
+            return routingType;
+        }
+        /// <summary>
+        /// Returns the response number for a given response option.
+        /// </summary>
+        /// <param name="respOption"></param>
+        /// <returns>int</returns>
+        public int ResponseNumber (string respOption)
+        {
+            Double d = 0;
+            string number="";
+            for (int i = 0; i < respOption.Length; i++)
             {
-                list = Regex.Replace(list, "  ", " ");
+                if (!Double.TryParse(respOption.Substring(i, 1), out d))
+                {
+                    number += respOption.Substring(i, 1);
+                }
             }
-
-            return list;
+            return Int32.Parse(number);
         }
 
         public string FilterText { get => routingText; set => routingText = value; }
         public bool HasVar { get => hasVar; }
-        internal List<FilterVar> FilterVars { get => filterVars; set => filterVars = value; }
+        internal List<FilterVar> RoutingVars { get => routingVars; set => routingVars = value; }
     }
 }
 
